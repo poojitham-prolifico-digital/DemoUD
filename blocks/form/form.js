7718cffcaacb5b6e288a3458954786948433157b
poojitham-prolifico-digital <poojitham@prolifico.digital>
2025-10-23 00:26:27 +0530

    Update form.js

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -1,21 +1,21 @@
 export default function decorate(block) {
-  const jotformId = block.textContent?.trim();
-
-  if (!jotformId || !/^\d+$/.test(jotformId)) return;
-
   const iframe = document.createElement('iframe');
-  iframe.src = `https://form.jotform.com/${jotformId}?isIframeEmbed=1&referrer=${window.location.href}`;
+
+  iframe.src = 'https://form.jotform.com/240255957613258?isIframeEmbed=1&referrer=https://www.mountainhomeutah.com/air-conditioning/&aaid=88575735570976223831839858657348094379,54,Direct,null,desktop,null&originReferrer=&promo_code=&utm_params=utm_campaign-null,utm_content-null,utm_medium-null,utm_source-null,utm_term-null';
+
   iframe.title = 'Contact_Us';
   iframe.loading = 'lazy';
   iframe.allowTransparency = 'true';
   iframe.allowFullscreen = true;
   iframe.setAttribute('allow', 'geolocation; microphone; camera');
-  iframe.frameBorder = '0';
-  iframe.scrolling = 'no';
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('scrolling', 'no');
   iframe.style.minWidth = '100%';
-  iframe.style.maxWidth = '100%';
+  iframe.style.maxWidth = '800px';
   iframe.style.height = '666px';
   iframe.style.border = 'none';
+  iframe.style.display = 'block';
+  iframe.style.margin = '0 auto';
 
   block.innerHTML = '';
   block.appendChild(iframe);
